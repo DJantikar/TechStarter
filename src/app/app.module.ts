0a8d7adcd71b4b2bc9ddfcd2f92c07004a201be5
Kenn Pascascio <kenn370@gmail.com>
2017-09-24 16:48:44 -0400

    added the productpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductpageComponent } from './productpage/productpage.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: ProductpageComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductpageComponent
   ],
   imports: [
     BrowserModule
